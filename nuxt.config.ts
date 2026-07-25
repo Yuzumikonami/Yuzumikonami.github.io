7db3057ec48a5fcaa6e91493cdb0e61a59b9d9c9
Yuzumikonami <1706491377@qq.com>
2026-07-25 20:33:11 +0800

    准备使用nuxt力喵

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // 注册模块
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/ui'
+  ],
+  // 引入全局 CSS 文件
+  css: ['~/assets/css/main.css']
 })
