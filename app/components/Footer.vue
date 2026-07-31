3c35a5d193179550adf8d0ef26bf288906bb16ab
Yuzumikonami <1706491377@qq.com>
2026-07-31 18:10:06 +0800

    萌ICP备 20269113 抢到豪的力喵😋(bushi

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer
       class="text-center text-sm text-gray-400 dark:text-gray-500 py-4 border-t border-gray-200/50 dark:border-gray-700/50 mt-8">
-    © 2026 柚见小南 · 用 🐾 和 ❤️ 搭建喵w~
+    © 2026 柚见小南 · 用 🐾 和 ❤️ 搭建喵w~ | <a href="https://icp.gov.moe/?keyword=20269113" target="_blank">萌ICP备20269113号</a>
   </footer>
 </template>
